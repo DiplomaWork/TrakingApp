d0881447e65fc5bce3fdf6acc0107480c3542dc8
Dima Rudyi <dmutro.ryduy@gmail.com>
2015-03-01 23:05:44 +0200

    Signed-off-by: Dima Rudyi <dmutro.ryduy@gmail.com>

---
+++ TrackingApp/TrackingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrackingApp.Startup))]
+namespace TrackingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
